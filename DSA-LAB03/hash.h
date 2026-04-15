58768b4914fb3475f76c71e04a573cb17df8a868
Hassnain-cs <mhassnain3547@conestogac.on.ca>
2026-04-15 19:23:56 -0400

    Added Header file hash.h

--- DSA-LAB03/hash.h
+++ DSA-LAB03/hash.h
@@ -1,1 +1,19 @@
 #pragma once
+#pragma once
+#ifndef HASH_H
+#define HASH_H
+
+/*
+ * This function is responsible for creating and filling a hash table.
+ * We are using double hashing to handle collisions.
+ *
+ * hashTable → the array where final hashed values will be stored
+ * input     → array of values we want to insert
+ * M         → size of the table (and number of elements)
+ * R         → used in second hash function (step size)
+ *
+ * We assume hashTable is already allocated in main.
+ */
+void hashInit(int* hashTable, int* input, int M, int R);
+
+#endif
